78a772d81244d182a63ad2007e5b6b7a88af8de1
EmptySet <bodnalev@gmail.com>
2023-11-30 14:28:11 +0000

    XDECREF fix

--- src/csdp_python.c
+++ src/csdp_python.c
@@ -12,32 +12,25 @@ PyObject* convert_blockrec_to_python_list(struct blockrec *block) {
 		return NULL;
 	}
 	for (int i = 1; i <= block->blocksize; ++i) {
-		PyObject *sublist = PyList_New(block->blocksize);
-		if (!sublist) {
-			Py_DECREF(result);
-			PyErr_SetString(PyExc_MemoryError, "Failed to create Python list");
-			return NULL;
-		}
-		for (int j = 1; j <= block->blocksize; ++j) {
-			value = 0.0;
-			if (block->blockcategory==MATRIX){
-				value = block->data.mat[ijtok(i,j,block->blocksize)];
-			}
-			else{
-				if (i==j){
-					value = block->data.vec[i];
-				}
-			}
-			PyObject *py_value = PyFloat_FromDouble(value);
-			if (!py_value) {
+		if (block->blockcategory==MATRIX){
+			PyObject *sublist = PyList_New(block->blocksize);
+			if (!sublist) {
 				Py_DECREF(result);
-				Py_DECREF(sublist);
-				PyErr_SetString(PyExc_MemoryError, "Failed to create Python float");
+				PyErr_SetString(PyExc_MemoryError, "Failed to create Python list");
 				return NULL;
 			}
-			PyList_SET_ITEM(sublist, j - 1, py_value);
+			for (int j = 1; j <= block->blocksize; ++j) {
+				value = block->data.mat[ijtok(i,j,block->blocksize)];
+				PyObject *py_value = PyFloat_FromDouble(value);
+				PyList_SET_ITEM(sublist, j - 1, py_value);
+			}
+			PyList_SET_ITEM(result, i - 1, sublist);
 		}
-		PyList_SET_ITEM(result, i - 1, sublist);
+		else{
+			value = block->data.vec[i];
+			PyObject *py_value = PyFloat_FromDouble(value);
+			PyList_SET_ITEM(result, i - 1, py_value);
+		}
 	}
 	return result;
 }
@@ -48,13 +41,13 @@ PyObject* convert_blockmatrix_to_python_list(struct blockmatrix *matrix) {
 		PyErr_SetString(PyExc_MemoryError, "Failed to create Python list");
 		return NULL;
 	}
-	for (int i = 0; i < matrix->nblocks; ++i) {
+	for (int i = 1; i <= matrix->nblocks; ++i) {
 		PyObject *block_list = convert_blockrec_to_python_list(&matrix->blocks[i]);
 		if (!block_list) {
 			Py_DECREF(result);
 			return NULL;
 		}
-		PyList_SET_ITEM(result, i, block_list);
+		PyList_SET_ITEM(result, i - 1, block_list);
 	}
 	return result;
 }
@@ -65,59 +58,18 @@ PyObject* c_solve_sdp(PyObject* self, PyObject* args) {
 	PyObject *mat_inds_obj;
 	PyObject *mat_vals_obj;
 	
-	if (!PyArg_ParseTuple(args, "OOOO", &block_sizes_obj, &a_obj, &mat_inds_obj, &mat_vals_obj)){
-		PyErr_SetString(PyExc_ValueError, "CSDPY couldn't parse the input");
-		return NULL;
-	}
+	PyObject *result_obj;
+	PyObject *X_obj;
+	PyObject *y_obj;
+	PyObject *Z_obj;
+	PyObject *pobj_obj;
+	PyObject *dobj_obj;
+	PyObject *ret_obj;
 	
-	int block_num = (int) PyObject_Length(block_sizes_obj);
-	int k = (int) PyObject_Length(a_obj);
-	int rows = (int) PyObject_Length(mat_vals_obj);
-	
-	
-	
-	int* block_sizes = (int*)malloc(block_num * sizeof(int));
-	if (!block_sizes){
-		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
-		return NULL;
-	}
-	for (int i = 0; i < block_num; i++) {
-		block_sizes[i] = (int)PyLong_AsLong(PyList_GetItem(block_sizes_obj, i));
-	}
-	
-	
-	
-	double* a = (double*)malloc((k + 1) * sizeof(double)); /* idk why the list is shifted by 1 */
-	if (!a){
-		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
-		return NULL;
-	}
-	for (int i = 1; i <= k; i++) {
-		a[i] = (double)PyFloat_AsDouble(PyList_GetItem(a_obj, i-1));
-	}
-	
-	
-	int* mat_inds = (int*)malloc(4 * rows * sizeof(int));
-	if (!mat_inds){
-		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
-		return NULL;
-	}
-	for (int i = 0; i < 4 * rows; i++) {
-		mat_inds[i] = (int)PyLong_AsLong(PyList_GetItem(mat_inds_obj, i));
-	}
-	
-	
-	
-	double* mat_vals = (double*)malloc(rows * sizeof(double));
-	if (!mat_vals){
-		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
-		return NULL;
-	}
-	for (int i = 0; i < rows; i++) {
-		mat_vals[i] = (double)PyFloat_AsDouble(PyList_GetItem(mat_vals_obj, i));
-	}
-	
-	
+	int *block_sizes;
+	double *a;
+	int *mat_inds;
+	double *mat_vals;
 	
 	int ret;
 	int n;
@@ -127,105 +79,104 @@ PyObject* c_solve_sdp(PyObject* self, PyObject* args) {
 	double *y;
 	double pobj,dobj;
 	
-	ret=from_sparse_data(k,block_num,block_sizes,rows,mat_inds,mat_vals, &n, &C, &constraints, 1);
+	if (!PyArg_ParseTuple(args, "OOOO", &block_sizes_obj, &a_obj, &mat_inds_obj, &mat_vals_obj)){
+		PyErr_SetString(PyExc_ValueError, "Couldn't parse the input");
+		return NULL;
+	}
 	
-	free(block_sizes);
-	free(mat_inds);
-	free(mat_vals);
+	int block_num = (int) PyObject_Length(block_sizes_obj);
+	int k = (int) PyObject_Length(a_obj);
+	int rows = (int) PyObject_Length(mat_vals_obj);
+	
+	
+	block_sizes = (int*)malloc(block_num * sizeof(int));
+	if (!block_sizes){
+		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
+		return NULL;
+	}
+	for (int i = 0; i < block_num; i++) {
+		block_sizes[i] = (int)PyLong_AsLong(PyList_GetItem(block_sizes_obj, i));
+	}
+	
+	
+	a = (double*)malloc((k + 1) * sizeof(double));
+	if (!a){
+		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
+		free(block_sizes);
+		return NULL;
+	}
+	for (int i = 1; i <= k; i++) {
+		a[i] = (double)PyFloat_AsDouble(PyList_GetItem(a_obj, i-1));
+	}
+	
+	
+	mat_inds = (int*)malloc(4 * rows * sizeof(int));
+	if (!mat_inds){
+		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
+		free(block_sizes);
+		free(a);
+		return NULL;
+	}
+	for (int i = 0; i < 4 * rows; i++) {
+		mat_inds[i] = (int)PyLong_AsLong(PyList_GetItem(mat_inds_obj, i));
+	}
+	
+	
+	mat_vals = (double*)malloc(rows * sizeof(double));
+	if (!mat_vals){
+		PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory");
+		free(block_sizes);
+		free(a);
+		free(mat_inds);
+		return NULL;
+	}
+	for (int i = 0; i < rows; i++) {
+		mat_vals[i] = (double)PyFloat_AsDouble(PyList_GetItem(mat_vals_obj, i));
+	}
+	
+	
+	
+	ret = from_sparse_data(k,block_num,block_sizes,rows,mat_inds,mat_vals, &n, &C, &constraints, 1);
 	
 	if (ret != 0)
 	{
 		PyErr_SetString(PyExc_ValueError, "Couldn't create an SDP from the data");
-		free_prob(n,k,C,a,constraints,X,y,Z);
+		free(block_sizes);
+		free(a);
+		free(mat_inds);
+		free(mat_vals);
 		return NULL;
 	}
 	
 	initsoln(n,k,C,a,constraints,&X,&y,&Z);
+	ret = easy_sdp(n,k,C,a,constraints,0.0,&X,&y,&Z,&pobj,&dobj);
 	
-	ret=easy_sdp(n,k,C,a,constraints,0.0,&X,&y,&Z,&pobj,&dobj);
+	result_obj = PyDict_New();
 	
-	PyObject* result_obj = PyDict_New();
-	if (!result_obj){
-		PyErr_SetString(PyExc_MemoryError, "Failed to create Python dictionary");
-		free_prob(n,k,C,a,constraints,X,y,Z);
-		return NULL;
-	}
-	
-	
-	PyObject* X_obj = convert_blockmatrix_to_python_list(&X);
-	if (!X_obj){
-		PyErr_SetString(PyExc_ValueError, "Couldn't convert the result to python objects");
-		free_prob(n,k,C,a,constraints,X,y,Z);
-		Py_DECREF(result_obj);
-		return NULL;
-	}
-	
-	
-	PyObject* Z_obj = convert_blockmatrix_to_python_list(&Z);
-	if (!Z_obj){
-		PyErr_SetString(PyExc_ValueError, "Couldn't convert the result to python objects");
-		free_prob(n,k,C,a,constraints,X,y,Z);
-		Py_DECREF(result_obj);
-		Py_DECREF(X_obj);
-		return NULL;
-	}
-	
-	
-	PyObject* y_obj = PyList_New(k);
-	if (!y_obj){
-		PyErr_SetString(PyExc_ValueError, "Couldn't convert the result to python objects");
-		free_prob(n,k,C,a,constraints,X,y,Z);
-		Py_DECREF(result_obj);
-		Py_DECREF(X_obj);
-		Py_DECREF(Z_obj);
-		return NULL;
-	}
+	X_obj = convert_blockmatrix_to_python_list(&X);
+	Z_obj = convert_blockmatrix_to_python_list(&Z);
+	y_obj = PyList_New(k);
 	for (int i=1; i<=k; i++){
 		PyObject *py_value = PyFloat_FromDouble(y[i]);
-		if (!py_value){
-			PyErr_SetString(PyExc_ValueError, "Couldn't convert the result to python objects");
-			free_prob(n,k,C,a,constraints,X,y,Z);
-			Py_DECREF(result_obj);
-			Py_DECREF(X_obj);
-			Py_DECREF(Z_obj);
-			Py_DECREF(y_obj);
-			return NULL;
-		}
 		PyList_SET_ITEM(y_obj, i - 1, py_value);
 	}
 	
-	PyObject* pobj_obj = PyFloat_FromDouble(pobj);
-	if (!pobj_obj){
-		PyErr_SetString(PyExc_ValueError, "Couldn't convert the result to python objects");
+	pobj_obj = PyFloat_FromDouble(pobj);
+	dobj_obj = PyFloat_FromDouble(dobj);
+	ret_obj = PyLong_FromLong(ret);
+	
+	if (!X_obj || !Z_obj || !y_obj || !pobj_obj || !dobj_obj || !ret_obj || !result_obj){
+		free(block_sizes);
+		free(mat_inds);
+		free(mat_vals);
 		free_prob(n,k,C,a,constraints,X,y,Z);
-		Py_DECREF(result_obj);
-		Py_DECREF(X_obj);
-		Py_DECREF(Z_obj);
-		Py_DECREF(y_obj);
-		return NULL;
-	}
-	PyObject* dobj_obj = PyFloat_FromDouble(dobj);
-	if (!dobj_obj){
-		PyErr_SetString(PyExc_ValueError, "Couldn't convert the result to python objects");
-		free_prob(n,k,C,a,constraints,X,y,Z);
-		Py_DECREF(result_obj);
-		Py_DECREF(X_obj);
-		Py_DECREF(Z_obj);
-		Py_DECREF(y_obj);
-		Py_DECREF(pobj_obj);
-		return NULL;
-	}
-	PyObject* ret_obj = PyLong_FromLong(ret);
-	if (!ret_obj){
-		PyErr_SetString(PyExc_ValueError, "Couldn't convert the result to python objects");
-		free_prob(n,k,C,a,constraints,X,y,Z);
-		Py_DECREF(result_obj);
-		Py_DECREF(X_obj);
-		Py_DECREF(Z_obj);
-		Py_DECREF(y_obj);
-		Py_DECREF(pobj_obj);
-		Py_DECREF(dobj_obj);
-		return NULL;
+		Py_XDECREF(X_obj);
+		Py_XDECREF(Z_obj);
+		Py_XDECREF(y_obj);
+		Py_XDECREF(pobj_obj);
+		Py_XDECREF(dobj_obj);
+		Py_XDECREF(ret_obj);
+		Py_XDECREF(result_obj);
 	}
 	
 	PyDict_SetItemString(result_obj, "code", ret_obj);
@@ -235,21 +186,17 @@ PyObject* c_solve_sdp(PyObject* self, PyObject* args) {
 	PyDict_SetItemString(result_obj, "y", y_obj);
 	PyDict_SetItemString(result_obj, "Z", Z_obj);
 	
-	
+	free(block_sizes);
+	free(mat_inds);
+	free(mat_vals);
 	free_prob(n,k,C,a,constraints,X,y,Z);
-	/*Py_DECREF(X_obj);
-	Py_DECREF(Z_obj);
-	Py_DECREF(y_obj);
-	Py_DECREF(pobj_obj);
-	Py_DECREF(dobj_obj);
-	Py_DECREF(ret_obj);*/
-	fflush(stdout);
 	
+	fflush(stdout);
 	return result_obj;
 }
 
 PyMethodDef methods[] = {
-	{"solve_sdp", c_solve_sdp, METH_VARARGS, "Run the ccsdp solver"},
+	{"solve_sdp", c_solve_sdp, METH_VARARGS, "Run the csdp solver"},
 	{NULL, NULL, 0, NULL}
 };
 
