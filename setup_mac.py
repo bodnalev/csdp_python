406185ebe0e94b11ba825f52a20ddbe8093ec7b6
EmptySet <bodnalev@gmail.com>
2024-10-29 14:39:57 +0000

    linux and mac setup scripts

---
+++ setup_mac.py
@@ -0,0 +1,36 @@
+from setuptools import setup, Extension
+import glob
+import os
+
+os.environ["CC"] = "/opt/homebrew/opt/llvm/bin/clang"
+
+with open("README.md", "r") as fp:
+    long_desc = fp.read()
+
+source_files = glob.glob("./src/*.c")
+
+setup(
+    name='csdpy',
+    version='0.1',
+    author="Levente Bodnar",
+    author_email="bodnalev@gmail.com",
+    description="Python wrapper for CSDP",
+    long_description_content_type='text/markdown',
+    long_description=long_desc,
+    license="EPL",
+    python_requires='>=3.6',
+    ext_modules = [Extension(
+        "csdpy",
+        source_files,
+        include_dirs=["./include"],
+        extra_compile_args=[
+            "-m64", "-march=native", "-mtune=native", "-O3", "-ffast-math", "-fPIC",
+            "-fopenmp", "-Wall", "-DBIT64", "-DUSEOPENMP", "-std=c99",
+            "-DSETNUMTHREADS", "-DUSESIGTERM", "-DUSEGETTIME", "-I../include", "-g"
+        ],
+        extra_link_args=[
+            "-L/opt/homebrew/opt/libomp/lib",
+            "-L../src", "-llapack", "-lblas", "-lm", "-lomp"
+        ]
+    )]
+)
